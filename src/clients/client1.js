19f78fe915fbb2ed13da8b9b449d9b682a6361a6
Andre Santos <andre@dexdev.com.br>
2023-10-05 21:27:17 -0300

    feat: clients

---
+++ src/clients/client1.js
@@ -0,0 +1,46 @@
+// and then establishes a P2P connection it.
+// It will then send { msg: 'hello' } to the RPC server
+
+'use strict'
+const CLIENT_ID="CLIENT_1"
+const { PeerRPCClient }  = require('grenache-nodejs-http')
+const Link = require('grenache-nodejs-link')
+const OrderBuilders = require('../builders/order')
+const link = new Link({
+    grape: 'http://127.0.0.1:30001'
+})
+let peer
+
+const startPeer = () => {
+    link.start()
+    peer = new PeerRPCClient(link, {})
+    peer.init()
+}
+const broadcast = async (order) => {
+    console.log('broadcast', {order})
+    const link = new Link({
+        grape: 'http://127.0.0.1:40002'
+    })
+    link.start()
+    const connectedPeer = new PeerRPCClient(link, {})
+    connectedPeer.init()
+    await OrderBuilders.requestAnOrder(order, connectedPeer)
+}
+
+
+const run = async () => {
+    startPeer()
+    const buyOrder = OrderBuilders.makeAnOrder({
+        price: 20000,
+        quantity: 2,
+        clientId: CLIENT_ID,
+        side: 'Buy'
+    })
+
+    await OrderBuilders.requestAnOrder(buyOrder, peer)
+    await broadcast(buyOrder)
+}
+
+run()
+    .catch(e => console.error(CLIENT_ID, e))
+
